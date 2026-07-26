964001d16abbf7bdd9e0455fef7f05132c9050dc
aut-supanut <supanut.suri@ku.th>
2026-07-26 11:50:16 +0700

    Add files via upload

--- BoundedStack.java
+++ BoundedStack.java
@@ -7,9 +7,10 @@ import java.util.*;
  * คำนามธรรม (A): ลำดับประวัติการค้นหา เช่น [การค้นหาA , การค้นหาB , การค้นหาC]
  */
 public class BoundedStack{
+     // ===== representation =====
 
-    private final List<String> search; //AF(search) = ลำดับประวัติการค้นหา 
-    private final int capacity;
+    private final List<String> searchs; //AF(search) = ลำดับประวัติการค้นหา 
+    private  final int capacity;
 
     // Representation Invariant:
     //   -มีประวัติการค้นหาอยู่จริงไม่เป็น null
@@ -18,27 +19,74 @@ public class BoundedStack{
     //   -ประวัติการค้นหาไม่เป็น null
     //   -ประวัติการค้นหามีเขียนไม่ปล่อยว่าง
 
+    //
+    // Safety from rep exposure:
+    //    -capacity เป็น private final    
+
     /**
      * 
      * @param capacity จำนวนที่สามารถเก็บประวัติการค้นหาได้
      */
     public BoundedStack(int capacity){
-        this.search = new ArrayList<>();
+        this.searchs = new ArrayList<>();
         this.capacity = capacity;
+
+         
+        }
+
+    private void checkRep(){
+        assert searchs != null : "searchs not null";
+        assert searchs.size() <= capacity : "Songs not over 100 ";
+
+        Set<String> seen = new HashSet<>();
+        for(String s : searchs){
+            assert seen.add(s) : "ค้นหาซ้ำ"+s;
+            assert s!=null :"search is null";   
+            assert !(s=="") :"sea is ว่าง";
+        }
     }
-
-    /**
-     * 
-     * @param s
-     */
-    public void push(String s){
-
-
-    }
+    
 
      // ===== Creator =====
+     /**
+      * สร้างประวัติการค้นหาว่าง
+      */
+
+     /**
+      * สร้างลิสต์จากรายการที่ค้นหา
+      * 
+      * @param 
+      */
+
+
+
+    
+
+
      // ===== Mutators =====
+
+
      // ===== Observers =====
+    /**
+     * คืนจำนวนจำนวนการค้นหาในประวัติ
+     */
+    public int size() {
+        return searchs.size();
+    }
+    /**
+     * ตรวจว่ามีเพลงนี้อยู่หรือไม่
+     */
+    public boolean contains(String search) {
+        return searchs.contains(search);  
+    }
+    /**
+     * คืนรายชื่อเพลงทั้งหมดตามลำดับ
+     */
+    public List<String> searchs() {
+        return new ArrayList<>(searchs);   // แก้บรรทัดนี้
+    }
+
+
      // ===== Producer =====
 
 }
